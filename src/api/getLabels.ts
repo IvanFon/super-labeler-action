077bd643ac86223d0de585018981e4f13bbd32ca
kazuki.morozumi <kazuki.m777@gmail.com>
2021-08-18 18:49:30 +0900

    [Fix]ラベルが30個以上ある場合に取得できない問題を修正、100件まで取得できるように対応する

--- src/api/getLabels.ts
+++ src/api/getLabels.ts
@@ -5,7 +5,8 @@ export const getLabels = async ({
   client,
   repo,
 }: ApiProps): Promise<Labels> => {
-  const labels = await client.issues.listLabelsForRepo({ ...repo });
+  // TODO: ラベルが100件以上ある場合にページングに対応させる必要がある
+  const labels = await client.issues.listLabelsForRepo({ per_page: 100, ...repo });
   return labels.data.map((label) => ({
     name: label.name,
     description: label.description,
